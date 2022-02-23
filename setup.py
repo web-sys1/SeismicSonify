1b8ef1fa534092d9fd5c027ef5393e0430126509
Web Source (SRC) <web.system.management@gmail.com>
2022-02-23 16:32:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
-#!/usr/bin/env python3
-
 from setuptools import find_packages, setup
 
-setup(name='sonify', packages=find_packages())
+from sonify import __version__
+
+setup(
+    name='sonify',
+    version=__version__,
+    packages=find_packages(),
+    entry_points=dict(console_scripts='sonify = sonify.sonify:main'),
+)
