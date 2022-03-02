944bbe4273649131f882e5903e9b9ab46fa7d094
Web Source (SRC) <web.system.management@gmail.com>
2022-03-02 16:48:18 +0200

    Update __init__.py

--- sonify/__init__.py
+++ sonify/__init__.py
@@ -1,1 +1,20 @@
+import subprocess
+from pathlib import Path
+
+__version__ = subprocess.run(
+    [
+        'git',
+        '-C',
+        Path(__file__).resolve().parent,
+        'rev-parse',
+        '--short=7',
+        'HEAD',
+    ],
+    capture_output=True,
+    text=True,
+).stdout.strip()
+
+del subprocess
+del Path
+
 from .sonify import sonify
